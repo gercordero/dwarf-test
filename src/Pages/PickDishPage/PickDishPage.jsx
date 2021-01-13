b063eaf1ed070c9f294401467b7233cfcdbab9c6
German <gercordero111@gmail.com>
2021-01-13 21:43:02 +0100

    Added getDishAction logic to PickDishPage

--- src/Pages/PickDishPage/PickDishPage.jsx
+++ src/Pages/PickDishPage/PickDishPage.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+// Redux actions
+import { getDishAction } from "../../actions/dish-actions";
 
 const PickDishPage = () => {
-  return (
-    <section>
-      <h1>Pick Dish Page</h1>
-    </section>
-  );
+  // Redux state
+  const dispatch = useDispatch();
+  const randomDishState = useSelector((state) => state.dishGet);
+
+  // We need to destructure dish so we now if we already got them before
+  const { dish } = randomDishState;
+
+  useEffect(() => {
+    // If we already have a dish no need to get another one again :).
+    if (!dish) {
+      dispatch(getDishAction());
+    }
+  }, [dispatch, dish]);
+
+  return <section></section>;
 };
 
 export default PickDishPage;
---
+++ src/actions/dish-actions/index.js
@@ -0,0 +1,1 @@
+export { default as getDishAction } from "./getDishAction";
