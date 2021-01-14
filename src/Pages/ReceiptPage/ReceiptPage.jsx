53ff67c2da25cc1ade697a4e6fc66aaa3f9dea9a
German <gercordero111@gmail.com>
2021-01-14 22:23:31 +0100

    Added redirect logic to OrderPage and ReceiptPage

--- src/Pages/ReceiptPage/ReceiptPage.jsx
+++ src/Pages/ReceiptPage/ReceiptPage.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+// Redux
+import { useSelector } from "react-redux";
 
-const ReceiptPage = () => {
+const ReceiptPage = ({ history }) => {
+  // Redux state
+  const { drinks } = useSelector((state) => state.drinksStore); // We need drinks to check for redirect.
+
+  // If user haven't selected a drink yet shouldn't be able to access this route
+  useEffect(() => {
+    if (drinks.length === 0) {
+      history.push("/pick-drink");
+    }
+  }, [history, drinks]);
   return (
     <section>
       <h1>Receipt Page</h1>
