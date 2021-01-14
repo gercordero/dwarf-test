6ef7f9c5b37dd553dad4954849f665b98a0f1563
German <gercordero111@gmail.com>
2021-01-14 15:41:10 +0100

    Added Get Drinks Action logic to PickDrinkPage

---
+++ src/Pages/PickDrinkPage/ContentGrid/ContentGrid.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContentGrid = ({ drinks }) => {
+  return <div></div>;
+};
+
+export default ContentGrid;
--- src/Pages/PickDrinkPage/PickDrinkPage.jsx
+++ src/Pages/PickDrinkPage/PickDrinkPage.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+// Redux actions
+import { getDrinksAction } from "../../actions/drinks-actions";
+// Components
+import { Display } from "../../Components";
+import ContentGrid from "./ContentGrid/ContentGrid";
 
 const PickDrinkPage = () => {
+  // Redux state
+  const dispatch = useDispatch();
+  const drinksState = useSelector((state) => state.drinksGet);
+
+  // We need to destructure drinks so we know if we already got them before
+  const { drinks } = drinksState;
+
+  useEffect(() => {
+    // If we already have a drinks no need to get another one again :).
+    if (!drinks) {
+      dispatch(getDrinksAction());
+    }
+  }, [dispatch, drinks]);
+
   return (
     <section>
-      <h1>Pick Drink Page</h1>
+      <Display
+        {...drinksState}
+        render={() => <ContentGrid drinks={drinks} />}
+      />
     </section>
   );
 };
