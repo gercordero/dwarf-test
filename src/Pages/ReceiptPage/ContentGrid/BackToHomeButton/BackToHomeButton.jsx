1bd2c070df35251ab1b179004c74405feb99185f
German <gercordero111@gmail.com>
2021-01-19 05:14:08 +0100

    Added reset action to back to home button

--- src/Pages/ReceiptPage/ContentGrid/BackToHomeButton/BackToHomeButton.jsx
+++ src/Pages/ReceiptPage/ContentGrid/BackToHomeButton/BackToHomeButton.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+// React router
+import { useHistory } from "react-router-dom";
 // Redux
 import { useDispatch } from "react-redux";
 // Redux actions
+import { resetAction } from "../../../../actions";
 // Styled components
 import {
   StyledGrid,
@@ -13,14 +16,18 @@ const BackToHomeButton = () => {
   // Redux state
   const dispatch = useDispatch();
 
+  // Use history
+  let history = useHistory();
+
+  const handleClick = () => {
+    dispatch(resetAction());
+    history.push("/");
+  };
+
   return (
     <StyledGrid item xs={12} sm={12} md={12}>
       <ButtonContainer>
-        <StyledButton
-          variant="contained"
-          color="primary"
-          onClick={() => console.log("Hello")}
-        >
+        <StyledButton variant="contained" color="primary" onClick={handleClick}>
           Back To Home
         </StyledButton>
       </ButtonContainer>
